f99097d7284aaf167b78db85486135761d51f7ee
Lee Reilly <lee@leereilly.net>
2012-02-04 15:02:11 -0800

    Train-driven development :train:

--- test/test_email.rb
+++ test/test_email.rb
@@ -1,7 +1,7 @@
 require 'helper'
 
 class TestEmail < Test::Unit::TestCase
-  should "know what a valid acadmic institution email address or domain is" do
+  should "work with all the things" do
     assert_equal Swot::is_academic?('lreilly@stanford.edu'),          true
     assert_equal Swot::is_academic?('lreilly@slac.stanford.edu'),     true
     assert_equal Swot::is_academic?('lreilly@strath.ac.uk'),          true
@@ -16,19 +16,22 @@ class TestEmail < Test::Unit::TestCase
     assert_equal Swot::is_academic?('lee@stanford.edu.com'),          false
     assert_equal Swot::is_academic?('lee@strath.ac.uk.com'),          false
 
-    assert_equal Swot::is_academic?('stanford.edu'),          true
-    assert_equal Swot::is_academic?('slac.stanford.edu'),     true
-    assert_equal Swot::is_academic?('strath.ac.uk'),          true
-    assert_equal Swot::is_academic?('soft-eng.strath.ac.uk'), true
-    assert_equal Swot::is_academic?('ugr.es'),                    true
-    assert_equal Swot::is_academic?('uottawa.ca'),                true
-    assert_equal Swot::is_academic?('mother.edu.ru'),             true
-    assert_equal Swot::is_academic?('ucy.ac.cy'),                 true
+    assert_equal Swot::is_academic?('stanford.edu'),                  true
+    assert_equal Swot::is_academic?('slac.stanford.edu'),             true
+    assert_equal Swot::is_academic?('www.stanford.edu'),              true
+    assert_equal Swot::is_academic?('http://www.stanford.edu'),       true
+    #assert_equal Swot::is_academic?('http://www.stanford.edu:9393'), true
+    assert_equal Swot::is_academic?('strath.ac.uk'),                  true
+    assert_equal Swot::is_academic?('soft-eng.strath.ac.uk'),         true
+    assert_equal Swot::is_academic?('ugr.es'),                        true
+    assert_equal Swot::is_academic?('uottawa.ca'),                    true
+    assert_equal Swot::is_academic?('mother.edu.ru'),                 true
+    assert_equal Swot::is_academic?('ucy.ac.cy'),                     true
 
-    assert_equal Swot::is_academic?('leerilly.net'),              false
-    assert_equal Swot::is_academic?('gmail.com'),                 false
-    assert_equal Swot::is_academic?('stanford.edu.com'),          false
-    assert_equal Swot::is_academic?('strath.ac.uk.com'),          false
+    assert_equal Swot::is_academic?('leerilly.net'),                  false
+    assert_equal Swot::is_academic?('gmail.com'),                     false
+    assert_equal Swot::is_academic?('stanford.edu.com'),              false
+    assert_equal Swot::is_academic?('strath.ac.uk.com'),              false
 
   end
 end
